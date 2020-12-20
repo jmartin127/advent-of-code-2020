c8112abdba206c948b70a3f05d3ff4be7b1b5902
jmartin127 <jmartin127@gmail.com>
2020-12-20 09:37:32 -0700

    first row id good

--- day20/part2/main.go
+++ day20/part2/main.go
@@ -38,6 +38,18 @@ func newPuzzle(size int) *puzzle {
 	}
 }
 
+func (p *puzzle) print() {
+	for _, r := range p.matrix {
+		for _, t := range r {
+			if t != nil {
+				t.print()
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+}
+
 func newTile(id int) *tile {
 	image := make([]*row, 0)
 	return &tile{
@@ -74,6 +86,7 @@ func (t *tile) addRow(r *row) {
 }
 
 func (t *tile) print() {
+	fmt.Printf("Tile: %d\n", t.id)
 	for _, r := range t.image {
 		for _, v := range r.vals {
 			if v {
@@ -144,7 +157,8 @@ func main() {
 		for j := 0; j < len(tiles); j++ {
 			if i != j {
 				t2 := tiles[j]
-				if numSharedEdges(t1, t2) > 0 {
+				numShared, _, _ := numSharedEdges(t1, t2)
+				if numShared > 0 {
 					numMatchesForTile++
 				}
 			}
@@ -170,23 +184,131 @@ func main() {
 	fmt.Printf("Num total %d\n", len(corners)+len(middle)+len(edges))
 
 	// Grab a corner and start building!
-	// Add matching edge pieces, the number to add would be: edge_size - 2
+	// Need to orient the corner so that a matching edge is to the right and left
 	firstCorner := corners[0]
-	match := findMatchingEdgePiece(firstCorner, edges)
-
 	fmt.Printf("Starting piece %d\n", firstCorner.id)
-	fmt.Printf("Match %d\n", match.id)
+	firstCorner = orientStartingPiece(firstCorner, edges)
+	firstCorner = rotateTile90DegressLeft(firstCorner)
+	firstCorner = rotateTile90DegressLeft(firstCorner)
+	firstCorner = rotateTile90DegressLeft(firstCorner)
+	firstCorner = flipTileHorizontal(firstCorner)
 
+	p := newPuzzle(size)
+	p.matrix[0][0] = firstCorner
+	remainingCorners := removeTileFromSlice(firstCorner, corners)
+	fmt.Printf("Starting piece\n")
+	firstCorner.print()
+
+	// Build out the top edge
+	remainingEdges, remainingCorners := buildTopEdge(firstCorner, p, edges, remainingCorners)
+	fmt.Printf("Num edges after building first edge %d\n", len(remainingEdges))
+	fmt.Printf("Num corners after building first edge %d\n", len(remainingCorners))
+	fmt.Printf("Puzzle after doing top edge:\n")
+	p.print()
+
+	// // Put the rest of the pieces in the box
+	// remaining := make([]*tile, 0)
+	// remaining = addToSlice(remaining, middle)
+	// remaining = addToSlice(remaining, remainingCorners)
+	// remaining = addToSlice(remaining, remainingEdges)
+
+	// // Build each row
+	// remaining = addRowToPuzzle(p, 1, remaining)
+	// fmt.Printf("Puzzle after doing middle row:\n")
+	// p.print()
 }
 
-func buildTopEdge(t *tile, edges []*tile) {
+func orientStartingPiece(t *tile, edges []*tile) *tile {
+	var count int
+	max := 4
+	rotatedTile := t
 
+	for true {
+		var foundOrientationOne bool
+		var foundOrientationThree bool
+		for _, e := range edges {
+			numMatch, _, orientationID := numSharedEdges(rotatedTile, e)
+			if numMatch > 0 {
+				fmt.Printf("orientation %d\n", orientationID)
+				if orientationID == 1 {
+					foundOrientationOne = true
+				} else if orientationID == 3 {
+					foundOrientationThree = true
+				}
+			}
+		}
+		if foundOrientationOne && foundOrientationThree {
+			return rotatedTile
+		}
+
+		rotatedTile.print()
+		rotatedTile = rotateTile90DegressLeft(rotatedTile)
+		rotatedTile.print()
+
+		count++
+		if count > max {
+			fmt.Println("You messed up Jeff")
+			os.Exit(1)
+		}
+	}
+
+	return nil
 }
 
-func findMatchingEdgePiece(t *tile, edges []*tile) *tile {
-	for _, edge := range edges {
-		if numSharedEdges(t, edge) > 0 {
-			return edge
+func addRowToPuzzle(p *puzzle, rowIndex int, pieces []*tile) []*tile {
+	remainingPieces := pieces
+
+	for i := 0; i < len(p.matrix); i++ {
+		match := findMatchingPiece(p.matrix[rowIndex-1][i], remainingPieces)
+		p.matrix[rowIndex][i] = match
+		remainingPieces = removeTileFromSlice(match, remainingPieces)
+	}
+
+	return remainingPieces
+}
+
+func addToSlice(o []*tile, toAdd []*tile) []*tile {
+	return append(o, toAdd...)
+}
+
+// Find edge_size - 2 edge pieces, starting with t, add them to the puzzle
+// ... return the updated list of edges
+func buildTopEdge(t *tile, p *puzzle, edges []*tile, corners []*tile) ([]*tile, []*tile) {
+	remainingEdges := edges
+	numEdgesToFind := len(p.matrix) - 2
+	lastPiecePlayed := t
+	for i := 0; i < numEdgesToFind; i++ {
+		match := findMatchingPiece(lastPiecePlayed, remainingEdges)
+		p.matrix[0][i+1] = match // set the edge piece in the puzzle
+		lastPiecePlayed = match
+
+		// remove the edge from the list of possible edges
+		remainingEdges = removeTileFromSlice(match, remainingEdges)
+	}
+
+	// add the top-right corner piece
+	match := findMatchingPiece(lastPiecePlayed, corners)
+	p.matrix[0][len(p.matrix)-1] = match // set the top-right corner piece in the puzzle
+	remainingCorners := removeTileFromSlice(match, corners)
+
+	return remainingEdges, remainingCorners
+}
+
+func removeTileFromSlice(t *tile, tiles []*tile) []*tile {
+	result := make([]*tile, 0)
+	for _, t2 := range tiles {
+		if t.id != t2.id {
+			result = append(result, t2)
+		}
+	}
+	return result
+}
+
+func findMatchingPiece(t *tile, tiles []*tile) *tile {
+	for _, tile := range tiles {
+		numShared, newTile, _ := numSharedEdges(t, tile)
+		if numShared > 0 {
+			return newTile
 		}
 	}
 
@@ -199,81 +321,74 @@ func findMatchingEdgePiece(t *tile, edges []*tile) *tile {
 // THEN flip
 //   2nd normal
 //   2nd rotated
-func numSharedEdges(t1 *tile, t2 *tile) int {
-	//fmt.Printf("COMPARE %d, %d\n", t1.id, t2.id)
-
-	// a := rotateTile90DegressLeft(t1)
-	// a = flipTileHorizontal(a)
-	// fmt.Println("A")
-	// a.print()
-	// b := flipTileHorizontal(t2)
-	// fmt.Println("B")
-	// b.print()
-
-	// r := compareTiles(a, b)
-	// fmt.Printf("R VALUE: %d\n", r)
-
-	if r := compareTiles(t1, t2); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+// Returns:
+// 1) the number of shared edges
+// 2) the matching tile, oriented properly
+// 3) the ID of HOW the match was made (see compareTiles)
+func numSharedEdges(t1 *tile, t2 *tile) (int, *tile, int) {
+	if r, id := compareTiles(t1, t2); r > 0 {
+		return r, t2, id
 	}
 	rotated := rotateTile90DegressLeft(t2)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 	rotated = rotateTile90DegressLeft(rotated)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 	rotated = rotateTile90DegressLeft(rotated)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 
 	flippedT2 := flipTileHorizontal(t2)
-	if r := compareTiles(t1, flippedT2); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, flippedT2); r > 0 {
+		return r, flippedT2, id
 	}
 	rotated = rotateTile90DegressLeft(flippedT2)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 	rotated = rotateTile90DegressLeft(rotated)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 	rotated = rotateTile90DegressLeft(rotated)
-	if r := compareTiles(t1, rotated); r > 0 {
-		//fmt.Printf("Num Match: %d\n", r)
-		return r
+	if r, id := compareTiles(t1, rotated); r > 0 {
+		return r, rotated, id
 	}
 
-	//fmt.Printf("Num Match: %d\n", -1)
-	return -1
+	return -1, nil, 0
 }
 
-func compareTiles(t1 *tile, t2 *tile) int {
-	var numMatching int
+// Returns the number of matching edges
+// ALSO returns HOW the edges matched (which edge ID)
+// 1 == right edge of 1
+// 2 == left edge of 1
+// 3 == bottom edge of 1
+// 4 == top edge of 1
+func compareTiles(t1 *tile, t2 *tile) (int, int) {
+	//var numMatching int
 	if compare1(t1, t2) {
-		numMatching++
+		return 1, 1
 	}
 	if compare2(t1, t2) {
-		numMatching++
+		return 1, 2
 	}
 	if compare3(t1, t2) {
-		numMatching++
+		return 1, 3
 	}
 	if compare4(t1, t2) {
-		numMatching++
+		return 1, 4
 	}
 
-	return numMatching
+	// // Should only ever be at most 1
+	// if numMatching > 1 {
+	// 	os.Exit(1)
+	// }
+
+	return 0, 0
 }
 
 func compare1(t1 *tile, t2 *tile) bool {
